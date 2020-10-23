a71fe649a36a7e97db3eb62cee2261dbfd430086
Gnxela <qawswedfrgtt@gmail.com>
2020-10-23 19:58:01 +0100

    Changed output image format to png. Jpg has lossy compression that I don't want.

--- src/main/java/me/alexng/worldGen/Main.java
+++ src/main/java/me/alexng/worldGen/Main.java
@@ -13,22 +13,21 @@ public class Main {
 		CombinedMap worldMap = new CombinedMap(new Sampler(10000, 10000));
 		CombinedMap sampledWorldMap = worldMap.sample(1000, 1000);
 		sampledWorldMap.generate(0);
-		writeMapDataToJpg(sampledWorldMap.getGenerationPipeline().getLandmassPipe().getStoredData(), ColorMaps.GREY_SCALE, "maps/landmass.jpg");
-		writeMapDataToJpg(sampledWorldMap.getGenerationPipeline().getMountainPipe().getStoredData(), ColorMaps.GREY_SCALE, "maps/mountain.jpg");
-		writeMapDataToJpg(sampledWorldMap.getGenerationPipeline().getHeightPipe().getStoredData(), ColorMaps.HEIGHT_MAP, "maps/height.jpg");
-		writeMapDataToJpg(sampledWorldMap.getGenerationPipeline().getMoisturePipe().getStoredData(), ColorMaps.MOISTURE_MAP, "maps/moisture.jpg");
-		writeMapDataToJpg(sampledWorldMap.getGenerationPipeline().getTemperaturePipe().getStoredData(), ColorMaps.TEMPERATURE_MAP, "maps/temperature.jpg");
-		writeMapDataToJpg(sampledWorldMap.getGenerationPipeline().getBiomePipe().getStoredData(), ColorMaps.BIOME_MAP, "maps/biome.jpg");
+		writeMapDataToPng(sampledWorldMap.getGenerationPipeline().getLandmassPipe().getStoredData(), ColorMaps.GREY_SCALE, "maps/landmass.png");
+		writeMapDataToPng(sampledWorldMap.getGenerationPipeline().getMountainPipe().getStoredData(), ColorMaps.GREY_SCALE, "maps/mountain.png");
+		writeMapDataToPng(sampledWorldMap.getGenerationPipeline().getHeightPipe().getStoredData(), ColorMaps.HEIGHT_MAP, "maps/height.png");
+		writeMapDataToPng(sampledWorldMap.getGenerationPipeline().getMoisturePipe().getStoredData(), ColorMaps.MOISTURE_MAP, "maps/moisture.png");
+		writeMapDataToPng(sampledWorldMap.getGenerationPipeline().getTemperaturePipe().getStoredData(), ColorMaps.TEMPERATURE_MAP, "maps/temperature.png");
+		writeMapDataToPng(sampledWorldMap.getGenerationPipeline().getBiomePipe().getStoredData(), ColorMaps.BIOME_MAP, "maps/biome.png");
 	}
 
-	private static void writeMapDataToJpg(MapData mapData, ColorMaps.ColorMap colorMap, String outputPath) throws IOException {
+	private static void writeMapDataToPng(MapData mapData, ColorMaps.ColorMap colorMap, String outputPath) throws IOException {
 		int width = mapData.getWidth();
 		int height = mapData.getHeight();
-		MemoryImageSource mis = new MemoryImageSource(width, height, colorMap.packToPixels(mapData.getRawData()), 0, width);
-		Image im = Toolkit.getDefaultToolkit().createImage(mis);
-
+		MemoryImageSource imageSource = new MemoryImageSource(width, height, colorMap.packToPixels(mapData.getRawData()), 0, width);
+		Image image = Toolkit.getDefaultToolkit().createImage(imageSource);
 		BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-		bufferedImage.getGraphics().drawImage(im, 0, 0, null);
-		ImageIO.write(bufferedImage, "jpg", new File(outputPath));
+		bufferedImage.getGraphics().drawImage(image, 0, 0, null);
+		ImageIO.write(bufferedImage, "png", new File(outputPath));
 	}
 }
